7128604d9a0c8685739572ce9c95c6fd8800c504
dantoniazzo <85890036+dantoniazzo@users.noreply.github.com>
2024-11-09 17:14:45 +0100

    Feature: add hamburger

--- src/App.tsx
+++ src/App.tsx
@@ -19,6 +19,8 @@ const Blur = styled.div`
 
 const Content = styled.div`
   position: absolute;
+  width: 100%;
+  height: 100%;
   left: 0;
   top: 0;
 `;
--- src/_widgets/Navbar/ui/Navbar.tsx
+++ src/_widgets/Navbar/ui/Navbar.tsx
@@ -1,18 +1,26 @@
 import styled from 'styled-components';
 import logo from '/logo.png';
+import hamburger from '/hamburger.png';
+import { PADDING } from '../lib/constants';
 
 const NavbarContainer = styled.div`
-  width: 100%;
+  width: calc(100% - ${PADDING * 2}px);
+  margin: ${PADDING}px;
   height: fit-content;
-  margin: 16px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 `;
 
 const Logo = styled.img``;
+
+const Hamburguer = styled.img``;
 
 export const Navbar = () => {
   return (
     <NavbarContainer>
       <Logo src={logo} />
+      <Hamburguer src={hamburger} />
     </NavbarContainer>
   );
 };
